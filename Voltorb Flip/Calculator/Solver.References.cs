f7a168286ee0236eac83355afc7c38784a174d72
TheRealWeeLad <mateoferntys@gmail.com>
2024-07-04 18:33:11 -0500

    Made reference boards public and static

--- Voltorb Flip/Calculator/Solver.References.cs
+++ Voltorb Flip/Calculator/Solver.References.cs
@@ -38,7 +38,7 @@ namespace Voltorb_Flip.Calculator
         readonly Color topLeftUnselectedBorderColor;
 
         // Reference Boards - (x2s, x3s, xVs)
-        readonly (int, int, int)[][] PossibleBoards = new (int, int, int)[][]
+        public static readonly (int, int, int)[][] PossibleBoards = new (int, int, int)[][]
         {
             new (int, int, int)[] { (3, 1, 6), (0, 3, 6), (5, 0, 6), (2, 2, 6), (4, 1, 6) }, // Level 1
             new (int, int, int)[] { (1, 3, 7), (6, 0, 7), (3, 2, 7), (0, 4, 7), (5, 1, 7) }, // Level 2
